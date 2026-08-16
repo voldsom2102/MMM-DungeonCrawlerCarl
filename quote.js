03dff46131ef49847afb58e772fc4a81220b5c59
voldsom2102 <soccer2102@gmail.com>
2026-08-16 11:48:29 -0500

    Enhance DCC_QUOTES with bookNumber property
    Added bookNumber property to each quote object.

--- quote.js
+++ quote.js
@@ -2,16 +2,19 @@ var DCC_QUOTES = [
     {
         text: "Goddammit Donut!",
         character: "Carl",
-        book: "Dungeon Crawler Carl"
+        book: "Dungeon Crawler Carl",
+        bookNumber: 1
     },
     {
         text: "He screamed, right in my fucking face",
         character: "Carl",
-        book: "Dungeon Crawler Carl"
+        book: "Dungeon Crawler Carl",
+        bookNumber: 1
     },
     {
         text: "Mongo is appaled",
         character: "Donut",
-        book: "The Butcher's Masqurade"
+        book: "The Butcher's Masqurade",
+        bookNumber: 2
     }
 ];
