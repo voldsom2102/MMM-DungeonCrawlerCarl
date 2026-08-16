38112f11d91a6a028fd52ff8d7e5467f011b0407
voldsom2102 <soccer2102@gmail.com>
2026-08-16 18:03:33 -0500

    Added More quotes

--- quote.js
+++ quote.js
@@ -1,19 +1,178 @@
 var DCC_QUOTES = [
     {
-        text: "Goddammit Donut!",
-        character: "Carl",
-        book: "Dungeon Crawler Carl"
+        text: "Okay, buddy. We gotta be quick. You are balls deep in the wrong hole, and mom is pulling into the driveway. You get me?",
+        character: "Quasar",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 5
     },
     {
-        text: "He screamed, right in my fucking face",
+        text: "Having a judge stick her finger up your ass is not the same as being interviewed for a television show",
         character: "Carl",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 1
+    },
+    {
+        text: "HE HAS AN ERECTION, MORDECAI! IT'S VERY INAPPROPRIATE! MONGO IS APPALLED!",
+        character: "Donut",
         book: "Dungeon Crawler Carl",
         bookNumber: 2
     },
     {
-        text: "Mongo is appaled",
+        text: "I will kill your Mother",
+        character: "Samantha",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 4
+    },
+    {
+        text: "God Dammit Donut!",
+        character: "Carl",
+        book: "Dungeon Crawler Carl"
+    },
+    {
+        text: "I bet he sucks his thumb and thinks of his grandmother when he touches himself.",
         character: "Donut",
-        book: "The Butcher's Masqurade",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 4
+    },
+    {
+        text: "Question: What’s the only thing standing between an innocent child and a happy, fulfilling life? Answer: You. The answer is you.",
+        character: "AI",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 1
+    },
+    {
+        text: "New Achievement",
+        character: "AI",
+        book: "Dungeon Crawler Carl"
+    },
+    {
+        text: "You look like you were dragged head first through the makeup aisle at the dollar store",
+        character: "Donut",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 6
+    },
+    {
+        text: "You will not break me.",
+        character: "Carl",
+        book: "Dungeon Crawler Carl"
+    },
+    {
+        text: "WHY DIDN'T YOUR MOTHER DRIBBLE YOU BACK ON TO THE TRUCK STOP BATHROOM FLOOR RIZON!",
+        character: "Donut",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 6
+    },
+    {
+        text: "Carl, you keep getting disgusting tattoos. You might as well grow a beard and knock over a liquor store now. That’s your third tattoo in a week. You’re going to scare Mongo.",
+        character: "Donut",
+        book: "Dungeon Crawler Carl",
         bookNumber: 5
+    },
+    {
+        text: "I DON’T HAVE THUMBS, CARL.",
+        character: "Donut",
+        book: "Dungeon Crawler Carl"
+    },
+    {
+        text: "OH HONEY, YOU USED TO BE A SEX DOLL, I'M SURE YOU CAN HANDLE ALL MANNER OF INDIGNITY!",
+        character: "Donut",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 5
+    },
+    {
+        text: "DON'T SLUTSHAME ME!",
+        character: "Samantha",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 5
+    },
+    {
+        text: "Glurp, glurp motherfucker.",
+        character: "Carl",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 3
+    },
+    {
+        text: "Shut your titty hole!",
+        character: "Quasar",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 6
+    },
+    {
+        text: "There is honor in giving someone a meal, but I wish to cook a feast for the whole galaxy.",
+        character: "Milk",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 6
+    },
+    {
+        text: "He screamed … right in my goddamned ear",
+        character: "Carl",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 5
+    },
+    {
+        text: "YOU SMELL LIKE THE ASSHOLE OF A FELCH DEMON.",
+        character: "Samantha",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 4
+    },
+    {
+        text: "Nothing about this is okay. Goddamn it, there's nowhere to sit down and have a breakdown in here.",
+        character: "Katia",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 3
+    },
+    {
+        text: "Really, Katia. If you need to borrow a sanitary napkin, just ask.",
+        character: "Donut",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 3
+    },
+    {
+        text: "I wasn't just going to abandon you, Carl. Who do you think I am? Miss Beatrice?",
+        character: "Donut",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 3
+    },
+    {
+        text: "You tried to kill Katia you fucking bitch!",
+        character: "Donut",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 3
+    },
+    {
+        text: "I hope you said your prayers and brought the lube, because you about to get fucked from here to eternity.",
+        character: "Carl",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 3
+    },
+    {
+        text: "You've received a gold \"makeup sex is the best sex\" box",
+        character: "AI",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 4
+    },
+    {
+        text: "WARNING: you have a dick!",
+        character: "AI",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 6
+    },
+    {
+        text: "Reward? Bitches don't get rewards!",
+        character: "AI",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 1
+    },
+    {
+        text: "Reward? I said the ghost of Steve Irwin smiles down upon you",
+        character: "AI",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 1
+    },
+    {
+        text: "New Achievement! Total, Utter Failure.",
+        character: "AI",
+        book: "Dungeon Crawler Carl",
+        bookNumber: 4
     }
 ];
